503f6dbbfdceb780b59828d8852fa0934a7180a9
John Clark <socrates37@gmail.com>
2023-10-08 15:39:24 -0600

    fix code before/after ticks

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,17 @@
+const markdownIt = require('markdown-it');
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
+// https://github.com/11ty/eleventy-plugin-syntaxhighlight/issues/38
+const md = markdownIt({
+  html: true,
+});
+md.renderer.rules.code_inline = (tokens, idx, { langPrefix = '' }) => {
+  const token = tokens[idx];
+  return `<code class="${langPrefix}">${token.content}</code>`;
+};
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.setLibrary('md', md);
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("{,!(_site)/**/}*.png");
   eleventyConfig.addPassthroughCopy("src/prism.css");
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+// https://www.skcript.com/blog/disable-tailwind-prose-code
+const disabledCSS = {
+	'code::before': false,
+	'code::after': false,
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,njk}"],
   theme: {
-    extend: {},
+    extend: {
+			typography: {
+				DEFAULT: { css: disabledCSS },
+				sm: { css: disabledCSS },
+				lg: { css: disabledCSS },
+				xl: { css: disabledCSS },
+				'2xl': { css: disabledCSS },
+			},
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),

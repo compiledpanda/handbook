c02728bf21779251b289f2a9b26061db784418da
John Clark <socrates37@gmail.com>
2023-10-08 09:42:02 -0600

    fix layouts

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("**/*.png");
+  eleventyConfig.addPassthroughCopy("{,!(_site)/**/}*.png");
   return {
     dir: {
       data: "_data",
